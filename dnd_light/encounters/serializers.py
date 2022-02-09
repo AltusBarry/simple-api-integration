8c06c341dd7d7e5e9aeeb1c2a6f07ad93c2702ec
AltusBarry <sprtn120@gmail.com>
2022-02-09 14:37:21 +0200

    develop: Update serializer

--- dnd_light/encounters/serializers.py
+++ dnd_light/encounters/serializers.py
@@ -6,4 +6,4 @@ from rest_framework import serializers
 class MonsterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Monster
-        fields = ["name", "armor_class", "hit_points"]
+        fields = ["name", "armor_class", "hit_points", "weapon"]
